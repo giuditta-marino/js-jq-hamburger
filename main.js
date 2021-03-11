ced69a7b35d5ad71b5cbd372c36d37f081e18b72
Giuditta <giudittamarino89@gmail.com>
2021-03-11 09:55:01 +0100

    esercizio perfezionato

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // MOSTRO HAMBURGER MENU AL CLICK
   // SALVO IL DIV CON CLASS HAMBURGER MENU IN UNA VARIABILE
 var hamburgerMenu = $('.hamburger-menu');
+var chiusura = $('.hamburger-menu .close');
 
   // PRENDO EVENTO CLICK
-$('.header-right > a').click(function (){
+$('.header-right i.fa-bars').click(function (){
   // AL CLICK SULL'ICONA HAMBURGER AGGIUNGO CLASSE ACTIVE
   hamburgerMenu.addClass('active');
 });
 
 // NASCONDO MENU HAMBURGER AL CLICK SULLA X
-$('.close').click(function(){
+chiusura.click(function(){
   // AL CLICK SU CLOSE TOLGO LA CLASSE ACTIVE ALL'HAMBURGER MENU
   hamburgerMenu.removeClass('active');
 })
